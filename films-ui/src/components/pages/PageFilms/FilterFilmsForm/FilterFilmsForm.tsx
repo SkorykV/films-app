c30a2f33cf9b4d16b5779a00ca4f9e19a412dd59
Volodymyr Skoryk <volodymyr_skorik@gmail.com>
2021-05-07 01:10:05 +0300

    UI - fetch all films on search form reset

--- films-ui/src/components/pages/PageFilms/FilterFilmsForm/FilterFilmsForm.tsx
+++ films-ui/src/components/pages/PageFilms/FilterFilmsForm/FilterFilmsForm.tsx
@@ -1,6 +1,6 @@
 import { Button, Grid, TextField } from '@material-ui/core';
 import { useFormik } from 'formik';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { getValidator } from '../../../../helpers/validation';
 import { FilterFilmsDTO } from '../../../../models/Film';
 
@@ -14,14 +14,39 @@ const FilterFilmsForm: React.FC<{
   disabled: boolean;
   onSubmit: (filters: FilterFilmsDTO) => void;
 }) => {
+  const [wasSubmited, setWasSubmited] = useState(false);
+
+  const handleSubmit = useCallback(
+    (filters: FilterFilmsDTO) => {
+      if (!wasSubmited) {
+        setWasSubmited(true);
+      }
+
+      onSubmit(filters);
+    },
+    [wasSubmited, onSubmit]
+  );
+
   const formik = useFormik({
     initialValues: {
       title: '',
       actorName: '',
     },
     validate: getValidator(FilterFilmsDTO),
-    onSubmit,
+    onSubmit: handleSubmit,
   });
+
+  const onFormFieldChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      const updatedValues = { ...formik.values, [name]: value };
+      if (wasSubmited && !updatedValues.title && !updatedValues.actorName) {
+        onSubmit(updatedValues);
+      }
+      formik.handleChange(e);
+    },
+    [wasSubmited, formik, onSubmit]
+  );
 
   return (
     <form onSubmit={formik.handleSubmit}>
@@ -33,7 +58,7 @@ const FilterFilmsForm: React.FC<{
             name='title'
             label='Title'
             value={formik.values.title}
-            onChange={formik.handleChange}
+            onChange={onFormFieldChange}
             error={formik.touched.title && Boolean(formik.errors.title)}
             helperText={formik.touched.title && formik.errors.title}
             disabled={disabled}
@@ -46,7 +71,7 @@ const FilterFilmsForm: React.FC<{
             name='actorName'
             label='Actor Name'
             value={formik.values.actorName}
-            onChange={formik.handleChange}
+            onChange={onFormFieldChange}
             error={formik.touched.actorName && Boolean(formik.errors.actorName)}
             helperText={formik.touched.actorName && formik.errors.actorName}
             disabled={disabled}
